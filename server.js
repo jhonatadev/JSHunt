537aa06eeefa199cef7839991f5c623a3fa3fe44
jhonatadev <xxxonata@gmail.com>
2019-08-06 05:49:43 -0300

    retaforação && organiazção

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const requireDir = require("require-dir");
+const express = require('express')
+const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 
 // Iniciando o App
-const app = express();
+const app = express()
 
 // Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
-requireDir("./src/models");
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true })
+requireDir('./src/models')
 
-const Product = mongoose.model("Product");
+// Rotas
+app.use('/api', require('./src/routes'))
 
-// Primeira rota
-app.get("/", (req, res) => {
-  Product.create({
-    title: 'React Native',
-    description: 'Build native apps with React',
-    url: 'http://github.com/facebook/react-native'
-  });
-  return res.send("Hello People 🤠");
-});
-
-app.listen(3001);
+app.listen(3001)
