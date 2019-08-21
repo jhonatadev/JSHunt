f8f73413fd22b2b6604dd815fe0e53412634b291
jhonatadev <xxxonata@gmail.com>
2019-08-21 11:01:06 -0300

    Criação de registro

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
-const express = require("express");
+const express = require('express');
 const routes = express.Router();
 
 const ProductController = require('./controllers/ProductControllers')
 
-routes.get("/products", ProductController.index);
+routes.get('/products', ProductController.index)
+routes.post('/products', ProductController.store)
+
 
 module.exports = routes;
